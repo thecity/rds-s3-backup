4c121602c2ef0ba62f6b4e862414d49276afd970
Jonathan Owens <jonathan@onthecity.org>
2011-09-08 10:50:14 -0700

    Adding configuration file option so you can trigger manual dumps remotely without specifying all those ugly passwords.
    Also adding gitignore.

--- rds-s3-backup.rb
+++ rds-s3-backup.rb
@@ -6,32 +6,49 @@ require 'fog'
 class RdsS3Backup < Thor
   
   desc "s3_dump", "Runs a mysqldump from a restored snapshot of the specified RDS instance"
-  method_options :rds_instance_id       => :required, 
-                 :s3_bucket             => :required,
-                 :aws_access_key_id     => :required,
-                 :aws_secret_access_key => :required, 
-                 :mysql_database        => :required, 
-                 :mysql_username        => :required, 
-                 :mysql_password        => :required, 
-                 :dump_ttl              => 0,
-                 :dump_directory        => '/mnt/'
+  method_option :rds_instance_id
+  method_option :s3_bucket
+  method_option :aws_access_key_id
+  method_option :aws_secret_access_key
+  method_option :mysql_database
+  method_option :mysql_username
+  method_option :mysql_password
+  method_option :dump_ttl, :default => 0, :desc => "Number of old dumps to keep."
+  method_option :dump_directory, :default => '/mnt/', :desc => "Where to store the temporary sql dump file."
+  method_option :config_file, :desc => "YAML file of defaults for any option. Options given during execution override these."
 
   def s3_dump
-    rds        = Fog::AWS::RDS.new(:aws_access_key_id => options[:aws_access_key_id], 
-                                   :aws_secret_access_key => options[:aws_secret_access_key])
+    begin
+      if options[:config_file]
+        my_options = options.merge(YAML.load(File.read(options[:config_file]))) {|key, cmdopt, cfgopt| cmdopt}
+      end
+    rescue Exception => e
+      puts "Unable to read specified configuration file #{options[:config_file]}. Reason given: #{e}"
+      exit(1)
+    end
 
-    rds_server = rds.servers.get(options[:rds_instance_id])
+    reqd_options = %w(rds_instance_id s3_bucket aws_access_key_id aws_secret_access_key mysql_database mysql_username mysql_password)
+    nil_options = reqd_options.find_all{ |opt| my_options[opt].nil?}
+    if nil_options.count > 0
+      puts "No value provided for required option(s) #{nil_options.join(' ')} in either config file or options."
+      exit(1)
+    end
+    
+    rds        = Fog::AWS::RDS.new(:aws_access_key_id => my_options[:aws_access_key_id], 
+                                   :aws_secret_access_key => my_options[:aws_secret_access_key])
+
+    rds_server = rds.servers.get(my_options[:rds_instance_id])
     s3         = Fog::Storage.new(:provider => 'AWS', 
-                                  :aws_access_key_id => options[:aws_access_key_id], 
-                                  :aws_secret_access_key => options[:aws_secret_access_key], 
+                                  :aws_access_key_id => my_options[:aws_access_key_id], 
+                                  :aws_secret_access_key => my_options[:aws_secret_access_key], 
                                   :scheme => 'https')
-    s3_bucket  = s3.directories.get(options[:s3_bucket])
+    s3_bucket  = s3.directories.get(my_options[:s3_bucket])
 
     snap_name        = "s3-dump-snap-#{Time.now.to_i}"
     backup_server_id = "#{rds_server.id}-s3-dump-server"
 
     backup_file_name     = "#{rds_server.id}-mysqldump-#{Time.now.strftime('%Y-%m-%d-%H-%M-%S-%Z')}.sql.gz"
-    backup_file_filepath = File.join(options[:dump_directory], backup_file_name)
+    backup_file_filepath = File.join(my_options[:dump_directory], backup_file_name)
     
     rds_server.snapshots.new(:id => snap_name).save
     new_snap = rds_server.snapshots.get(snap_name)
@@ -44,8 +61,8 @@ class RdsS3Backup < Thor
     backup_server.wait_for { ready? }
 
     dump_result = `/usr/local/mysql/bin/mysqldump --opt --add-drop-table --single-transaction --order-by-primary \
-                  -h #{backup_server.endpoint['Address']} -u #{options[:mysql_username]} --password=#{options[:mysql_password]} \ 
-                  #{options[:mysql_database]} | gzip --fast -c > #{backup_file_filepath} 2>&1`
+                  -h #{backup_server.endpoint['Address']} -u #{my_options[:mysql_username]} --password=#{my_options[:mysql_password]} \ 
+                  #{my_options[:mysql_database]} | gzip --fast -c > #{backup_file_filepath} 2>&1`
     
     unless dump_result == ''
       puts "Dump failed with error #{dump_result}"
@@ -61,13 +78,15 @@ class RdsS3Backup < Thor
       
       backup_server.wait_for { ready? }
       backup_server.destroy(nil)
+      
+      File.unlink(backup_file_filepath)
     end
     
-    if options[:dump_ttl] > 0
+    if my_options[:dump_ttl] > 0
       my_files = s3_bucket.files.all('prefix' => "#{rds_server.id}-mysqldump-")
-      if my_files.count > options[:dump_ttl]
+      if my_files.count > my_options[:dump_ttl]
         files_by_date = my_files.sort {|x,y| x.created_at <=> y.created_at}
-        (files_by_date.count - options[:dump_ttl]).times do |i| 
+        (files_by_date.count - my_options[:dump_ttl]).times do |i| 
           files_by_date[i].destroy
         end
       end
