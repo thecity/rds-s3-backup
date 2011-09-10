6c568888999c676e4eb32f4e6a45abcf07eea406
Jonathan Owens <jonathan@onthecity.org>
2011-09-09 19:26:57 -0700

    Broke overlong task into methods. Optioned out s3 directory. Fixed bad backtick line-breaking. Clean up no matter what.

--- rds-s3-backup.rb
+++ rds-s3-backup.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
 require 'thor'
 require 'fog'
 
 class RdsS3Backup < Thor
   
-  desc "s3_dump", "Runs a mysqldump from a restored snapshot of the specified RDS instance"
+  desc "s3_dump", "Runs a mysqldump from a restored snapshot of the specified RDS instance, and uploads the dump to S3"
   method_option :rds_instance_id
   method_option :s3_bucket
+  method_option :s3_prefix, :default => 'db_dumps'
   method_option :aws_access_key_id
   method_option :aws_secret_access_key
   method_option :mysql_database
@@ -18,21 +20,7 @@ class RdsS3Backup < Thor
   method_option :config_file, :desc => "YAML file of defaults for any option. Options given during execution override these."
 
   def s3_dump
-    begin
-      if options[:config_file]
-        my_options = options.merge(YAML.load(File.read(options[:config_file]))) {|key, cmdopt, cfgopt| cmdopt}
-      end
-    rescue Exception => e
-      puts "Unable to read specified configuration file #{options[:config_file]}. Reason given: #{e}"
-      exit(1)
-    end
-
-    reqd_options = %w(rds_instance_id s3_bucket aws_access_key_id aws_secret_access_key mysql_database mysql_username mysql_password)
-    nil_options = reqd_options.find_all{ |opt| my_options[opt].nil?}
-    if nil_options.count > 0
-      puts "No value provided for required option(s) #{nil_options.join(' ')} in either config file or options."
-      exit(1)
-    end
+    my_options = build_configuration(options)
     
     rds        = Fog::AWS::RDS.new(:aws_access_key_id => my_options[:aws_access_key_id], 
                                    :aws_secret_access_key => my_options[:aws_secret_access_key])
@@ -60,19 +48,51 @@ class RdsS3Backup < Thor
     backup_server.wait_for { ready? }
     backup_server.wait_for { ready? }
 
-    dump_result = `/usr/local/mysql/bin/mysqldump --opt --add-drop-table --single-transaction --order-by-primary \
-                  -h #{backup_server.endpoint['Address']} -u #{my_options[:mysql_username]} --password=#{my_options[:mysql_password]} \ 
-                  #{my_options[:mysql_database]} | gzip --fast -c > #{backup_file_filepath} 2>&1`
+    dump_result = `mysqldump --opt --add-drop-table --single-transaction --order-by-primary -h #{backup_server.endpoint['Address']} -u #{my_options[:mysql_username]} --password=#{my_options[:mysql_password]} #{my_options[:mysql_database]} | gzip --fast -c > #{backup_file_filepath} 2>&1`
     
     unless dump_result == ''
       puts "Dump failed with error #{dump_result}"
+      cleanup(new_snap, backup_server, backup_file_filepath)
+      exit(1)
     end
     
-    if s3_bucket.files.new(:key => "db_dumps/#{backup_file_name}", 
+    if s3_bucket.files.new(:key => File.join(my_options[:s3_prefix], backup_file_name), 
                            :body => File.open(backup_file_filepath), 
                            :acl => 'private', 
                            :content_type => 'application/x-gzip'
                            ).save
+      if my_options[:dump_ttl] > 0
+       prune_dumpfiles(File.join(my_options[:s3_prefix], "#{rds_server.id}-mysqldump-"), my_options[:dump_ttl])
+      end   
+    else
+      puts "S3 upload failed!"                        
+    end
+
+    cleanup(new_snap, backup_server, backup_file_filepath)
+  end
+  
+  no_tasks do
+    def build_configuration(thor_options)
+      {}.tap do |merged_options|
+        begin
+          if options[:config_file]
+            merged_options = options.merge(YAML.load(File.read(options[:config_file]))) {|key, cmdopt, cfgopt| cmdopt}
+          end
+        rescue Exception => e
+          puts "Unable to read specified configuration file #{options[:config_file]}. Reason given: #{e}"
+          exit(1)
+        end
+
+        reqd_options = %w(rds_instance_id s3_bucket aws_access_key_id aws_secret_access_key mysql_database mysql_username mysql_password)
+        nil_options = reqd_options.find_all{ |opt| my_options[opt].nil?}
+        if nil_options.count > 0
+          puts "No value provided for required option(s) #{nil_options.join(' ')} in either config file or options."
+          exit(1)
+        end
+      end
+    end
+    
+    def cleanup(new_snap, backup_server, backup_file_filepath)
       new_snap.wait_for { ready? }
       new_snap.destroy
       
@@ -82,17 +102,17 @@ class RdsS3Backup < Thor
       File.unlink(backup_file_filepath)
     end
     
-    if my_options[:dump_ttl] > 0
-      my_files = s3_bucket.files.all('prefix' => "#{rds_server.id}-mysqldump-")
-      if my_files.count > my_options[:dump_ttl]
+    def prune_dumpfiles(backup_file_prefix, dump_ttl)
+      my_files = s3_bucket.files.all('prefix' => backup_file_prefix)
+      if my_files.count > dump_ttl
         files_by_date = my_files.sort {|x,y| x.created_at <=> y.created_at}
-        (files_by_date.count - my_options[:dump_ttl]).times do |i| 
+        (files_by_date.count - dump_ttl).times do |i| 
           files_by_date[i].destroy
         end
       end
     end
+    
   end
-  
 end
 
 RdsS3Backup.start
